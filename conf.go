0b03447af4dfde0a3fde016bbeff5e4e85fed49d
Jerry Bai <chinaren_bjr@163.com>
2016-07-17 08:40:16 +0800

    drop space line

--- conf.go
+++ conf.go
@@ -1,3 +1,6 @@
+/*
+Package conf is to provide a config, such as ini, json, xml etc.
+*/
 package conf
 
 import (
@@ -6,18 +9,30 @@ import (
 )
 
 var consoleLog = log.New(os.Stdout, "[conf] ", log.LstdFlags)
-var DEBUG bool = false
 
+// DEBUG is switcher for debug
+var DEBUG = false
+
+// Config is an interface for config
 type Config interface {
+	// Get value
 	Get(section, name string) string
+	// GetInt is to get interger value
 	GetInt(section, name string) int
+	// Set value
 	Set(section, name, value string)
+	// GetSection is to get section
 	GetSection(section string) []SectionConfig
+	// Reload config
 	Reload() error
 }
 
+// SectionConfig is section part for config
 type SectionConfig interface {
+	// Get value
 	Get(name string) string
+	//GetInt is to get interger value
 	GetInt(name string) int
+	// Set value
 	Set(name, value string)
 }
